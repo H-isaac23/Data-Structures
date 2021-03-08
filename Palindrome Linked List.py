72a29b037e111a359c7aa56ccceb693f65fb4991
John Isaac Delgado <ji23delgado@gmail.com>
2021-03-08 19:06:11 +0800

    Accepted solution

--- Palindrome Linked List.py
+++ Palindrome Linked List.py
@@ -20,3 +20,49 @@ The number of nodes in the list is in the range [1, 105].
 
 Follow up: Could you do it in O(n) time and O(1) space?"""
 
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        if head is None or head.next is None:
+            return True
+
+        length = 0
+        temp = head
+
+        while temp:
+            temp = temp.next
+            length += 1
+
+        if length % 2 == 0:
+            length = int(length / 2)
+        else:
+            length = int((length // 2) + 1)
+
+        temp = head
+        for i in range(length):
+            temp = temp.next
+
+        head2 = temp
+
+        while head2.next:
+            next_node = head2.next.next
+            head2.next.next = temp
+            temp = head2.next
+            head2.next = next_node
+
+        main = head
+
+        while temp:
+            if temp.val != main.val:
+                return False
+            temp = temp.next
+            main = main.next
+
+        return True
