730f5e5e906b72e95c214e7a3ef04a446a4166d0
John Isaac Delgado <ji23delgado@gmail.com>
2021-03-09 23:28:02 +0800

    Add func desc

--- Design Doubly LInked List.py
+++ Design Doubly LInked List.py
@@ -50,6 +50,11 @@ class MyLinkedList:
         self.size = 0
 
     def get(self, index: int) -> int:
+        """
+        Get the value of the index-th node in the linked list. If the index is invalid, return -1.
+        :type index: int
+        :rtype: int
+        """
         temp = self.head
 
         if index < 0 or index >= self.size:
@@ -61,6 +66,11 @@ class MyLinkedList:
         return temp.val
 
     def addAtHead(self, val: int) -> None:
+        """
+        Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
+        :type val: int
+        :rtype: None
+        """
         new_node = DNode(val)
         if self.head is None:
             self.head = new_node
@@ -74,6 +84,11 @@ class MyLinkedList:
         self.size += 1
 
     def addAtTail(self, val: int) -> None:
+        """
+        Append a node of value val to the last element of the linked list.
+        :type val: int
+        :rtype: None
+        """
         new_node = DNode(val)
         temp = self.head
 
@@ -88,6 +103,12 @@ class MyLinkedList:
         self.size += 1
 
     def addAtIndex(self, index: int, val: int) -> None:
+        """
+        Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
+        :type index: int
+        :type val: int
+        :rtype: None
+        """
         if index > self.size:
             return
 
@@ -109,6 +130,11 @@ class MyLinkedList:
             self.size += 1
 
     def deleteAtIndex(self, index: int) -> None:
+        """
+        Delete the index-th node in the linked list, if the index is valid.
+        :type index: int
+        :rtype: None
+        """
         temp = self.head
         if self.head is None or index >= self.size or index < 0:
             return
