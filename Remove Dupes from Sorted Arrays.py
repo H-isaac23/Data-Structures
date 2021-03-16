ed18325265dd1109e657ea8c415478f04c5df5c6
John Isaac Delgado <ji23delgado@gmail.com>
2021-03-16 23:35:30 +0800

    Accepted solution

--- Remove Dupes from Sorted Arrays.py
+++ Remove Dupes from Sorted Arrays.py
@@ -18,3 +18,23 @@ Constraints:
 0 <= nums.length <= 3 * 104
 -104 <= nums[i] <= 104
 nums is sorted in ascending order."""
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        count = 0
+        num_dup = 0
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i + 1]:
+                num_dup += 1
+
+        i = 0
+        while i < len(nums) - 1 and count < num_dup:
+            if i == len(nums) - 1:
+                break
+            elif nums[i] == nums[i + 1]:
+                nums.pop(i + 1)
+            else:
+                i += 1
+
+        return len(nums) - count
+    
